5b2b8da13c0bb4731072f22f48e6f1e79059a08d
Shafqat Bhatti <shafqatghafoor99@gmail.com>
2020-03-31 13:22:58 +0500

    post and get method

--- routes/web.php
+++ routes/web.php
@@ -83,6 +83,7 @@ Route::POST('searchChallan','Web\FeeDepositController@searchChallan')->name('sea
 
 Route::POST('feedeposit-paypal','Web\PaypalFeeDepositController@feeDepositpaypal')->name('feeDepositpaypal');
 Route::get('feedeposit-paypal-status','Web\PaypalFeeDepositController@getPaymentStatus')->name('payment.status');
+Route::GET('feedeposit-status','Web\FeeDepositController@feeDepositstatus')->name('feeDepositstatus');
 
 
 
