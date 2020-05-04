93650b5bc6ea7073a0d286eaee88c6b269ab9c33
shafqat <shafqatghafoor99@gmail.com>
2020-05-04 12:12:41 +0500

    new

--- resources/views/web/pakistan/howToPay.blade.php
+++ resources/views/web/pakistan/howToPay.blade.php
@@ -1,7 +1,235 @@
 @extends('_layouts.web.pakistan.default')
 @section('content')
+<div class="header_box" style="margin-top:140px;">
 
+</style>
+	
+</div>
+<p class="covid">Facing issue in fee submission in COVID-19 Pandemic?<br> Don't worry. You can locate your nearest JazzCash retailer by sending <strong>M</strong> in an SMS to <u>2179</u> or by calling <u>4444</u> from any Jazz number.</p>
+<div class="merit-bg p-5">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 text-center">
+                        <h1 class="text-white"> How to Pay for Your lyceumgroupofschools.com Subscription</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+ <section class="how-to-pay">
+            <div class="container">
+                <div class="row  align-items-center">
 
+                                                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+                                <div class="card my-4 p-3">
+                                    <h3 class="text-dark my-2 text-center d-block font-weight-bold">
+                                        Pay Via Mobi Cash                                    </h3>
+                                    <figure class="text-center">
+                                        <img class="img-fluid" src="{{asset('assets/frontend/images/jazz_new.jpg')}}" alt="err" data-pagespeed-url-hash="3944744305" onload="pagespeed.CriticalImages.checkImageForCriticality(this);"style="max-width: 180px;">
+                                    </figure>
+                                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                                        <p class="text-center"></p>
+                                        <ul class="list-group">
+<li class="list-group-item border-left-0 border-right-0"><i class="fa fa-check-square"></i> You can visit any MobiCash or Jazz Cash point or transfer amount from your jazzcash/mobicash account  to mobile number that is  mentioned below.</li>
+<li class="list-group-item border-left-0 border-right-0"><i class="fa fa-check-square"></i> MobiCash Number: <b>0300 8925082</b></li>
+</ul>                                    </div>
+                                    <!--                                    <video width="200" controls >
+                                        <source width="200" height="200" src="<?//= base_url() ?>assets/videos/how_to_pay_mobi_cash.mp4" type="video/mp4">
+                                    </video>-->
+                                </div>
+                            </div>
+                                                        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+                                <div class="card my-4 p-3">
+                                    <h3 class="text-dark my-2 text-center d-block font-weight-bold">
+                                        Pay Via Easy Paisa                                    </h3>
+                                    <figure class="text-center">
+                                        <img class="img-fluid" src="{{asset('assets/frontend/images/esypaisa.png')}}" alt="err" data-pagespeed-url-hash="2623594503" onload="pagespeed.CriticalImages.checkImageForCriticality(this);" style="max-width: 180px;">
+                                        	
+                                        </style>
+                                    </figure>
+                                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                                        <p class="text-center"></p>
+                                        <ul class="list-group">
+<li class="list-group-item border-left-0 border-right-0"><i class="fa fa-check-square"></i> You can visit any Easypaisa shop or transfer amount  from your Easypaisa account to mobile number that is  mentioned below.</li>
+<li class="list-group-item border-left-0 border-right-0"><i class="fa fa-check-square"></i> Easypaisa Number: <b>03008925082</b></li>
+</ul>                                    </div>
+                                    <!--                                    <video width="200" controls >
+                                        <source width="200" height="200" src="<?//= base_url() ?>assets/videos/how_to_pay_mobi_cash.mp4" type="video/mp4">
+                                    </video>-->
+                                </div>
+                            </div>
+                                                        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+                                <div class="card my-4 p-3">
+                                    <h3 class="text-dark my-2 text-center d-block font-weight-bold">
+                                        Pay Via Meezan Bank                                    </h3>
+                                    <figure class="text-center">
+                                        <img class="img-fluid" src="https://www.topgrade.pk/assets/images/payment_logos/xmeezan-bank.jpg.pagespeed.ic.26ybGerJUl.webp" alt="err" data-pagespeed-url-hash="437941469" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
+                                    </figure>
+                                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                                        <p class="text-center"></p>
+                                        <ul class="list-group">
+<li class="list-group-item border-left-0 border-right-0"><i class="fa fa-check-square"></i> Account Title : <b>TOP GRADE</b></li>
+<li class="list-group-item border-left-0 border-right-0"><i class="fa fa-check-square"></i> Account Number: <b>21010103155444</b></li>
+<li class="list-group-item border-left-0 border-right-0"><i class="fa fa-check-square"></i> IBAN: <b>PK58MEZN0021010103155444</b></li>
+</ul>                                    </div>
+                                    <!--                                    <video width="200" controls >
+                                        <source width="200" height="200" src="<?//= base_url() ?>assets/videos/how_to_pay_mobi_cash.mp4" type="video/mp4">
+                                    </video>-->
+                                </div>
+                            </div>
+                                                        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+                                <div class="card my-4 p-3">
+                                    <h3 class="text-dark my-2 text-center d-block font-weight-bold">
+                                        Pay Via Allied Bank                                    </h3>
+                                    <figure class="text-center">
+                                        <img class="img-fluid" src="https://www.topgrade.pk/assets/images/payment_logos/xabl.jpg.pagespeed.ic.snB_TCiHes.webp" alt="err" data-pagespeed-url-hash="3147205725" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
+                                    </figure>
+                                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                                        <p class="text-center"></p>
+                                        <ul class="list-group">
+<li class="list-group-item border-left-0 border-right-0"><i class="fa fa-check-square"></i> Account Title : <b>TOP GRADE</b></li>
+<li class="list-group-item border-left-0 border-right-0"><i class="fa fa-check-square"></i> Account Number: <b>12020010052159300011</b></li>
+<li class="list-group-item border-left-0 border-right-0"><i class="fa fa-check-square"></i> IBAN: <b>PK57ABPA0010052159300011</b></li>
+</ul>                                    </div>
+                                    <!--                                    <video width="200" controls >
+                                        <source width="200" height="200" src="<?//= base_url() ?>assets/videos/how_to_pay_mobi_cash.mp4" type="video/mp4">
+                                    </video>-->
+                                </div>
+                            </div>
+                                                        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+                                <div class="card my-4 p-3">
+                                    <h3 class="text-dark my-2 text-center d-block font-weight-bold">
+                                        Pay Via Cash On Delivery                                    </h3>
+                                    <figure class="text-center">
+                                        <img class="img-fluid" src="https://www.topgrade.pk/assets/images/payment_logos/xcod.jpg.pagespeed.ic.QaS2ilX8NB.webp" alt="err" data-pagespeed-url-hash="1581639262" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
+                                    </figure>
+                                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                                        <p class="text-center"></p>
+                                        <ul class="list-group"><li class="list-group-item border-left-0 border-right-0"><i class="fa fa-check-square"></i> Select Pay by Cash on Delivery as your payment method.</li><li class="list-group-item border-left-0 border-right-0"><i class="fa fa-check-square"></i> You will receive your account username and password by TCS Courier.</li><li class="list-group-item border-left-0 border-right-0"><i class="fa fa-check-square"></i> Pay the due amount to the TCS representative in cash.</li><li class="list-group-item border-left-0 border-right-0"><i class="fa fa-check-square"></i> Start using your account with the received username and password, for any help give us a call.</li></ul>                                    </div>
+                                    <!--                                    <video width="200" controls >
+                                        <source width="200" height="200" src="<?//= base_url() ?>assets/videos/how_to_pay_mobi_cash.mp4" type="video/mp4">
+                                    </video>-->
+                                </div>
+                            </div>
+                                                        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+                                <div class="card my-4 p-3">
+                                    <h3 class="text-dark my-2 text-center d-block font-weight-bold">
+                                        Pay Via Jazz Cash                                    </h3>
+                                    <figure class="text-center">
+                                        <img class="img-fluid" src="https://www.topgrade.pk/assets/images/payment_logos/xjazz-cash.jpg.pagespeed.ic.Ceh81qC9YZ.webp" alt="err" data-pagespeed-url-hash="795418191" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
+                                    </figure>
+                                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                                        <p class="text-center"></p>
+                                        <ul class="list-group"><li class="list-group-item border-left-0 border-right-0"><i class="fa fa-check-square"></i> Select Jazz Cash voucher as your payment option, you will receive a voucher code on your mobile and email.</li><li class="list-group-item border-left-0 border-right-0"><i class="fa fa-check-square"></i> Present this voucher code to the nearest Jazz Cash Point or any Jazz/Warid franchise and pay the due amount.</li><li class="list-group-item border-left-0 border-right-0"><i class="fa fa-check-square"></i> Your account will automatically activated as soon as the payment is received.</li></ul>                                    </div>
+                                    <!--                                    <video width="200" controls >
+                                        <source width="200" height="200" src="<?//= base_url() ?>assets/videos/how_to_pay_mobi_cash.mp4" type="video/mp4">
+                                    </video>-->
+                                </div>
+                            </div>
+                                                        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+                                <div class="card my-4 p-3">
+                                    <h3 class="text-dark my-2 text-center d-block font-weight-bold">
+                                        Pay Via Card payment                                    </h3>
+                                    <figure class="text-center">
+                                        <img class="img-fluid" src="https://www.topgrade.pk/assets/images/payment_logos/xcard-payment.jpg.pagespeed.ic.DKm2H91oIb.webp" alt="err" data-pagespeed-url-hash="3903371337" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
+                                    </figure>
+                                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                                        <p class="text-center"></p>
+                                                                            </div>
+                                    <!--                                    <video width="200" controls >
+                                        <source width="200" height="200" src="<?//= base_url() ?>assets/videos/how_to_pay_mobi_cash.mp4" type="video/mp4">
+                                    </video>-->
+                                </div>
+                            </div>
+                            
+                </div>
+            </div>
+        </section>
+<style>
+.how-to-pay .card {
+    box-shadow: 0 2px 4px 0 rgba(0,0,0,0.16),0 2px 10px 0 rgba(0,0,0,0.12)!important;
+}
+.p-3 {
+    padding: 1rem!important;
+}
+.mt-4, .my-4 {
+    margin-top: 1.5rem!important;
+}
+.d-block {
+    display: block!important;
+}
+.text-dark {
+    color: #343a40!important;
+}
+h3{
+    font-size: 32px;
+    line-height: 28px;}
+.font-weight-bold {
+    font-weight: 700!important;
+}
+h1 {
+    font-size: 38px;
+    line-height: 44px;
+    color: #282828;
+    font-family: 'proxima_novasemibold';
+}
+.img-fluid {
+    max-width: 100%;
+    height: auto;
+}
+.text-white {
+    color: #fff!important;
+}
+.list-group-item {
+    position: relative;
+    display: block;
+    padding: .75rem 1.25rem;
+    margin-bottom: -1px;
+    background-color: #fff;
+    border: 1px solid rgba(0,0,0,.125);
+}
+
+.card {
+    position: relative;
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-orient: vertical;
+    -webkit-box-direction: normal;
+    -ms-flex-direction: column;
+    flex-direction: column;
+    min-width: 0;
+    word-wrap: break-word;
+    background-color: #fff;
+    background-clip: border-box;
+    border: 1px solid rgba(0,0,0,.125);
+    border-radius: .25rem;
+}
+.covid {
+    background: #3169B9;
+    color: #fff;
+    padding: 2px 0;
+    font-size: 20px;
+    text-align: center;
+}
+.merit-bg {
+    background: url('{{asset('assets/frontend/images/banner.jpg')}}');
+    background-repeat: no-repeat;
+    background-size: cover!important;
+    position: relative;
+}
+.merit-bg:before {
+    position: absolute;
+    content: '';
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, .50);
+}
+.p-5 {
+    padding: 3rem!important;
+}
+</style>
 
 @endsection
 @push('post-scripts')
